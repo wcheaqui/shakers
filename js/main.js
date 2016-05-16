8c220c8ec5074f511ca5e9d2828a6ce3598284b6
William Cheaqui <willcheaqui@yahoo.com>
2016-05-15 23:07:35 -0700

    add details for jewelry

---
+++ js/main.js
@@ -0,0 +1,157 @@
+var jewelry1 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    priceMobile:"29.99"
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.',
+    more:"more/jewelry1.html"
+};
+
+jewelry1 = jewelry1.replace(/imageSource|caption|priceA|priceMobile|descriptionA|more/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry1);
+
+var jewelry2 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry2 = jewelry2.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry2);
+
+var jewelry3 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry3 = jewelry1.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry3);
+
+var jewelry4 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry4 = jewelry4.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry4);
+
+var jewelry5 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry5 = jewelry5.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry5);
+
+var jewelry6 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry6 = jewelry6.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry6);
+
+var jewelry7 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry7 = jewelry7.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry7);
+
+var jewelry8 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry8 = jewelry8.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry8);
+
+var jewelry9 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry9 = jewelry9.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry9);
+
+var jewelry10 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry10 = jewelry10.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry10);
+
+var jewelry11 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry11 = jewelry11.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry11);
+
+var jewelry12 = image;
+var mapOjb = {
+    imageSource:"http://ep.yimg.com/ay/wholesalecostumejewelry/w21245n-11.jpg",
+    caption:"Football Necklace",
+    priceA:"$29.99",
+    descriptionA:'Exclusively Ours! Lead, Nickel & Cadmium Free!! Detailed Silver Tone And Genuine Austrian Topaz And Clear Crystal Covered Heart-Shaped Football Charm Comes On A Bracelet Of Your Choice: Toggle Chain, Link Chain Or Black Suede Leatherette With Lobster Clasps.'
+};
+
+jewelry12 = jewelry12.replace(/imageSource|caption|priceA|descriptionA/gi, function(matched){
+    return mapOjb[matched];
+});
+$("#desktop").append(jewelry12);
